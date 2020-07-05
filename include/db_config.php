70de9ab31099416781e8d4d83588d6753c409941
Timo <t.zacherl@icloud.com>
2020-07-05 16:35:58 +0200

    updated formatting

--- include/db_config.php
+++ include/db_config.php
@@ -1,20 +1,23 @@
 <?php
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'root');
 define('DB_DATABASE', 'oop_log_mod');
 
-class DB_con {
-	public $connection;
-	function __construct(){
-		$this->connection = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD,DB_DATABASE);
-		
-		if ($this->connection->connect_error) die('Database error -> ' . $this->connection->connect_error);
-		
-	}
-	
-	function ret_obj(){
-		return $this->connection;
-	}
-
+class DB_con
+{
+    public $connection;
+    public function __construct()
+    {
+        $this->connection = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+        
+        if ($this->connection->connect_error) {
+            die('Database error -> ' . $this->connection->connect_error);
+        }
+    }
+    
+    public function ret_obj()
+    {
+        return $this->connection;
+    }
 }
